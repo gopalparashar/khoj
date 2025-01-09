88c2167b5334a06dd866b9084b2bf45d1b0f3e63
Gopal Parashar <118716031+gopalparashar@users.noreply.github.com>
2025-01-10 02:48:02 +0530

    Update settings.py

--- src/khoj/app/settings.py
+++ src/khoj/app/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import 
+import os
 from pathlib import Path
 
 from django.templatetags.static import static
